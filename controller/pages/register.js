d85bc64e3f0a02dbbd072c211d95b2d1eaa5f472
frxncismor <frxncismor@gmail.com>
2021-02-13 17:27:33 -0600

    Add: Register page

---
+++ controller/pages/register.js
@@ -0,0 +1,10 @@
+console.log('register works');
+
+const sendRegistration = (event) => {
+	event.preventDefault();
+	const username = document.getElementById('username').value;
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+
+	console.log({username, email, password});
+};
