e245d3578b6d00cc1cbff75ea4043ae08f2a20b9
Francisco J. Moreno <franciscoj.sparcoltary@gmail.com>
2021-02-22 00:40:48 -0600

    add: tabs and content relation

--- controller/pages/user-home.js
+++ controller/pages/user-home.js
@@ -2,3 +2,16 @@ const dropdownMenu = () => {
 	const dropdown = document.querySelector('.home__header--dropdown');
 	dropdown.classList.toggle('menu-active');
 };
+
+const openTab = (event, section_ticket) => {
+	const tickets_tabs = document.getElementsByClassName('tickets__tab');
+	const tablinks = document.getElementsByClassName('tabs__button');
+	const activeClass = `active-tab`;
+	for (let index = 0; index < tickets_tabs.length; index++) {
+		tickets_tabs[index].style.display = 'none';
+		tablinks[index].className = tablinks[index].className.replace(activeClass, '');
+	}
+	document.getElementById(section_ticket).style.display = 'block';
+	event.currentTarget.classList.add(activeClass);
+	event.currentTarget.classList.add(section_ticket);
+};
